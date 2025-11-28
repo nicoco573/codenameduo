a450f128e3fc9b37deff87aac95bc487607f4a1d
nicoco573 <nicolycee57.3@gmail.com>
2025-11-28 14:17:44 +0100

    Refactor Game class to use Scene instead of Screen

--- game.py
+++ game.py
@@ -1,22 +1,23 @@
 import tkinter as tkint
 
-from screen import Screen
+from scene import Scene
 
 
 class Game:
     def __init__(self):
         self.running = True
-        self.screen = Screen(640, 480)
+        self.scene = Scene(640, 480)
     
     
     def close_window(self):
         """Ferme le programme"""
         self.running = False            #arrete la mise à jour de l'affichage
-        self.screen.window.destroy()    #Ferme la fenêtre
+        self.scene.window.destroy()    #Ferme la fenêtre
     
     
     def run(self):
         """Lance le programme"""
-        self.screen.window.protocol("WM_DELETE_WINDOW", self.close_window)  # Appelle la fonction quand on ferme la fenêtre
+        self.scene.window.protocol("WM_DELETE_WINDOW", self.close_window)  # Appelle la fonction quand on ferme la fenêtre
         if self.running:
-            self.screen.update()    #Met à jour l'affichage
+            self.scene.update()    #Met à jour l'affichage
+
