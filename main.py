2fec757ba4c043e4fe51bf6cba7d64fbe31ae911
nicoco573 <nicolycee57.3@gmail.com>
2025-11-28 14:20:21 +0100

    Fix reference to scene in main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     game = Game()
     game.run()                      #lance le jeux
 
-    game.screen.window.mainloop()   #Boucle principale tkinter
+    game.scene.window.mainloop()   #Boucle principale tkinter
+
