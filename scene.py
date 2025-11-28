81737b50dec46c3de397682d2f765ca28d52c909
Rocket_ <RocketLauncher5717@gmail.com>
2025-11-28 14:53:40 +0100

    Update scene.py

--- scene.py
+++ scene.py
@@ -6,16 +6,28 @@ from gameboard import GameBoard
 
 class Scene:
     
-    def __init__(self, canva:tkint.Canvas, screen, name:str, gameboard:GameBoard=GameBoard(KeyPlayer(1),KeyPlayer(2), scene=None), clue=None):
-        self.canva :tkint.Canvas = canva
-        self.gameboard :GameBoard = gameboard
-        self.gameboard.scene = self
+    def __init__(self, canva:tkint.Canvas, name:str, width:int, height:int, clue=None):
+        self.canva :tkint.Canvas = canva       
+        self.gameboard :GameBoard = GameBoard(KeyPlayer(1),KeyPlayer(2))    # initialisation du gameboard
+        self.gameboard.scene = self                                         # scene du gameboard
         
-        self.screen = screen
+        # -- Paramètres de la fenêtre --
+        self.witdh = width
+        self.height = height
         
-        self.name = name
+        self.window = tkint.Tk(className="CodeName")    #initialisation de la fenêtre tkinter
+        self.window.geometry(f"{width}x{height}")       #taille de la fenêtre
         
-        self.clue :str | None = clue    #Indice donné par le joueur actif
+        self.canva :tkint.Canvas = tkint.Canvas(self.window, width=width, height=height, background="ivory")    # création du canva
+        self.canva.pack()   # affiche le canva
+        
+        # -- Scène actuelle, celle qui s'affiche --
+        self.current_scene :Scene = Scene(self.canva, self, "menu") # Scène acctuelle et initialisation de la scene. (Commence au menu)
+        
+        self.name = name                                            # Nom de la scène
+        
+        self.clue :str | None = clue                                # Indice donné par le joueur actif
+
 
     def update(self):
         """Met à jour l'affichage, efface/redessine affin d'afficher les bonnes choses"""
@@ -32,14 +44,14 @@ class Scene:
     def change_scene(self, new_name:str):
         """change le nom de la scène"""
         print(new_name)
-        self.name = new_name        #change le nom de la scene
-        self.gameboard.destroy_widgets()      #détruit les widgets du gameboard
-        self.update_current_scene() #initialise self.current_scene dans la classe screen
-        self.screen.update()        #Dessine la nouvelle scene
+        self.name = new_name                #change le nom de la scene
+        self.gameboard.destroy_widgets()    #détruit les widgets du gameboard
+        self.update_current_scene(new_name)         #initialise self.current_scene dans la classe screen
+        self.screen.update()                #Dessine la nouvelle scene
     
-    def update_current_scene(self):
+    def update_current_scene(self, name:str):
         """Met à jour la scène acctuelle"""
-        self.screen.current_scene = Scene(self.canva, self.screen, self.name, self.gameboard, self.clue)
+        self.current_scene = name
     
     def scene_elements(self):
         """Contient les éléments de chaques scènes à afficher
@@ -47,7 +59,7 @@ class Scene:
         Returns:
             list: liste des éléments à afficher
         """
-        
+        # -- MENU --
         if self.name == "menu": #Scène menu
             return [
                 self.canva.create_rectangle(0, 0, 640, 480, fill="lightblue"),
@@ -55,6 +67,7 @@ class Scene:
                 self.canva.create_window((320,380), window=tkint.Button(self.canva, text="Jouer",font=("Arial", 20,"bold"),command=lambda: self.change_scene("regles")), anchor="n"),
             ]
         
+        # -- REGLES --
         elif self.name == "regles": #Scène expliquant les rêgles
             return [
                 self.canva.create_rectangle(0, 0, 640, 480, fill="lightblue"),
@@ -69,21 +82,29 @@ class Scene:
                 self.canva.create_window((320,380), window=tkint.Button(self.canva, text="Suivant",font=("Arial", 20, 'bold'),command=lambda: self.change_scene("game1")), anchor="n"),
             ]
         
+        
+        # -- GAME 1 --
         elif self.name == "game1": #Scène du jeu : joueur 1
             # print(self.gameboard.player1.loose)
             print(self.gameboard.player2.win)
+            
+            # -- PERTE DU JOUEUR 2 --
             if self.gameboard.player2.loose:
                 return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
                     self.canva.create_text((300, 300), text="Vous avez perdu !\nC'est au joueur 1 de jouer.\nFaite le deviner !", font=("Arial", 27, "bold"), fill="red", anchor="s"),
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 2: clé", font=("Arial", 10), command=lambda: self.change_scene("key2")))
                 ]
+            
+            # -- VICTOIRE DU JOUEUR 2 --
             elif self.gameboard.player2.win:
                 return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
                     self.canva.create_text((300, 300), text="Vous avez trouvé tous vos agents !\nC'est au joueur 1 de jouer.\nFaite le deviner !", font=("Arial", 27, "bold"), fill="green", anchor="s"),
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 2: clé", font=("Arial", 10), command=lambda: self.change_scene("key2")))
                 ]
+            
+            # -- TRUC DE BASE --
             else:
                 return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
@@ -92,6 +113,8 @@ class Scene:
                     self.canva.create_text((20, 455), text=f"Indice : {self.clue if self.clue else 'Aucun Indice, regardez la clé !'}", font=("Arial", 15,'bold'), anchor="sw", fill='blue'),
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 2: clé", font=("Arial", 10), command=lambda: self.change_scene("key2"))),
                 ]
+        
+        # -- CLE DU JOUEUR 1 --
         elif self.name == "key1": #Scène de la clé du joueur 1
             entree_indice = tkint.Entry(self.canva, font=("Arial", 12), width=20)
             self.gameboard.widgets.append(entree_indice)
@@ -104,20 +127,28 @@ class Scene:
                 self.canva.create_text((320, 380), text="Entrez votre indice: ", font=("Arial", 11), anchor="s", fill='blue', justify="left")
             ]
         
+        
+        # -- GAME 2 --
         elif self.name == "game2": #Scène du jeu : joueur 2
             print(self.gameboard.player1.win)
+            
+            # -- PERTE DU JOUEUR 1 --
             if self.gameboard.player1.loose:
                 return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
                     self.canva.create_text((300, 300), text="Vous avez perdu !\nC'est au joueur 2 de jouer.\nFaite le deviner !", font=("Arial", 27, "bold"), fill="red", anchor="s"),
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 1: clé", font=("Arial", 10), command=lambda: self.change_scene("key1")))
                 ]
+            
+            # -- VICTOIRE DU JOUEUR 1 --
             elif self.gameboard.player1.win:
                 return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
                     self.canva.create_text((300, 300), text="Vous avez trouvé tous vos agents !\nC'est au joueur 2 de jouer.\nFaite le deviner !", font=("Arial", 27, "bold"), fill="green", anchor="s"),
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 1: clé", font=("Arial", 10), command=lambda: self.change_scene("key1")))
                 ]
+                
+            # -- TRUC DE BASE --
             else:
                 return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
@@ -127,6 +158,9 @@ class Scene:
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 1: clé", font=("Arial", 10), command=lambda: self.change_scene("key1"))),
                 ]
         
+        
+        
+        # -- CLE DU JOUEUR 2 --
         elif self.name == "key2": #Scène de la clé du joueur 2
             entree_indice = tkint.Entry(self.canva, font=("Arial", 12), width=20)
             self.gameboard.widgets.append(entree_indice)
@@ -139,26 +173,28 @@ class Scene:
                 self.canva.create_text((320, 380), text="Entrez votre indice: ", font=("Arial", 11), anchor="s", justify="left", fill='blue')
             ]
         
+        
+        # -- ERREUR COMMISE PAR LE JOUEUR 1 --
         elif self.name == "mistake1":
             return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
-                    self.canva.create_text((20, 430), text="Joueur 2, devine !", font=("Arial", 12), anchor="sw", fill='green'),
-                    self.canva.create_text((20, 455), text=f"Indice : {self.clue if self.clue else 'Aucun Indice, regardez la clé !'}", font=("Arial", 15,'bold'), anchor="sw", fill='blue'),
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 1: clé", font=("Arial", 10), command=lambda: self.change_scene("key1"))),
                     self.canva.create_rectangle((220,440),(420,480), fill="lightgrey", tag="info"),
                     self.canva.create_text((320, 460), text="Tu t'es trompé !\nFin de ton tour.", font=("Arial", 15), fill="red", tag="info")
                 ]
         
+        
+        # -- ERREUR COMMISE PAR LE JOUEUR 2 --
         elif self.name == "mistake2":
             return [
                     self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
-                    self.canva.create_text((20, 430), text="Joueur 1, devine !", font=("Arial", 12), anchor="sw", fill='green'),
                     self.canva.create_text((20, 455), text=f"Indice : {self.clue if self.clue else 'Aucun Indice, regardez la clé !'}", font=("Arial", 15,'bold'), anchor="sw", fill='blue'),
                     self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 2: clé", font=("Arial", 10), command=lambda: self.change_scene("key2"))),
                     self.canva.create_rectangle((220,440),(420,480), fill="lightgrey", tag="info"),
                     self.canva.create_text((320, 460), text="Tu t'es trompé !\nFin de ton tour.", font=("Arial", 15), fill="red", tag="info")
                 ]
-            
+        
+        # -- PERTE DU JOUEUR 1 --
         elif self.name == "loose1":
             return [
                 self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
@@ -166,6 +202,7 @@ class Scene:
                 self.canva.create_window((620, 440), anchor="se", window=tkint.Button(self.canva, text="Joueur 1: clé", font=("Arial", 10), command=lambda: self.change_scene("key1")))
             ]
         
+        # -- PERTE DU JOUEUR 2 --
         elif self.name == "loose2":
             return [
                 self.canva.create_rectangle((0,0),(640,480), fill="lightblue"),
@@ -198,22 +235,8 @@ class Scene:
                 return  # quitte la fonction, attend que l'utilisateur clique à nouveau
         if self.name == "key1":
             self.change_scene("game1")
+        
         elif self.name == "key2":
-
             self.change_scene("game2")
 
-class Screen:
-    def __init__(self, width:int, height:int):
-        self.witdh = width
-        self.height = height
-        
-        self.window = tkint.Tk(className="CodeName")    #initialisation de la fenêtre tkinter
-        self.window.geometry(f"{width}x{height}")       #taille de la fenêtre
-        
-        self.canva :tkint.Canvas = tkint.Canvas(self.window, width=width, height=height, background="ivory")
-        self.canva.pack()   #affiche le canva
-        
-        self.current_scene :Scene = Scene(self.canva, self, "menu") #scène acctuelle, celle qui s'affiche
-    
-    
 
